ffb9cb8465e653ce1d4aac60d96e1d96067ac2b8
mel-chiu <melinda.hsinying.chiu@gmail.com>
2020-06-06 21:46:58 +0200

    Try to correct the audio path in order to play it on GitHub as well.

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ window.addEventListener('keypress', function (e) {
 function pianoPlay(key) {
     switch (key) {
         case "C":
-            let audioC = new Audio("/audio/piano_middle_C.mp3");
+            let audioC = new Audio("../audio/piano_middle_C.mp3");
             audioC.play();
             break;
         case "D":
-            let audioD = new Audio("/audio/piano_D.mp3");
+            let audioD = new Audio("../audio/piano_D.mp3");
             audioD.play();
             break;
         case "E":
-            let audioE = new Audio("/audio/piano_E.mp3");
+            let audioE = new Audio("../audio/piano_E.mp3");
             audioE.play();
             break;
         case "F":
-            let audio = new Audio("/audio/piano_F.mp3");
+            let audio = new Audio("../audio/piano_F.mp3");
             audio.play();
             break;
         case "G":
-            let audioG = new Audio("/audio/piano_G.mp3"); //The audio is a bit brokern
+            let audioG = new Audio("../audio/piano_G.mp3"); //The audio is a bit brokern
             audioG.play();
             break;
         case "A":
-            let audioA = new Audio("/audio/piano_A.mp3");
+            let audioA = new Audio("../audio/piano_A.mp3");
             audioA.play();
             break;
         case "B":
-            let audioB = new Audio("/audio/piano_B.mp3");
+            let audioB = new Audio("../audio/piano_B.mp3");
             audioB.play();
             break;
         default: console.log("Let's click a button!")
